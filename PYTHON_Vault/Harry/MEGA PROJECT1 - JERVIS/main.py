72d5b1296df2b5df5f05b89881f887943e41f97d
labib <labib.hsn.bd@gmail.com>
2026-01-30 00:28:07 +0600

    update web learing file

---
+++ PYTHON_Vault/Harry/MEGA PROJECT1 - JERVIS/main.py
@@ -0,0 +1,35 @@
+import speech_recognition as sr
+import pyttsx3
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak("Initializing Jersvis...")
+
+    while True:
+        # Listen for the wake word "Jarvis"
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+         
+        print("recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Ya")
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+
+        except Exception as e:
+            print("Error; {0}".format(e))
