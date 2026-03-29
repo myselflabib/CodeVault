fb42bacbaaa6727954d26fbf74e02d076c841bda
labib <labib.hsn.bd@gmail.com>
2026-03-29 22:46:22 +0600

    added Translation code of CF

--- ONLINE_JUDGE/CODEFORCES/__800/A_Translation.cpp
+++ ONLINE_JUDGE/CODEFORCES/__800/A_Translation.cpp
@@ -1,22 +1,32 @@
-#include<bits/stdc++.h>
+///   ***   ---   ||   SOLVED BY LABIB   ||   ---   ***   ///
+///   ***   ---   ||   DATE: 29/03/2026  ||   ---   ***   ///
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define debug(x) cout << #x << " --> " << x << endl;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); 
+    cout.tie(0);
+
     string s, t;
     cin >> s >> t;
 
     int flag = 0;
     
-    string w = s;
-    reverse(w.begin(), w.end());
+    reverse(s.begin(), s.end());
 
-    if(w!=t){
-        cout << "NO" << endl;
-    }
-
-    else{
-        cout << "YES" << endl;
-    }
+    if(s!=t) cout << "NO" << endl;
+    else cout << "YES" << endl;
     
+
     return 0;
 }
