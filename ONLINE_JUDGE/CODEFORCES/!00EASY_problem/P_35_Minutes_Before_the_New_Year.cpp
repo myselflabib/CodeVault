280ade3d7033323b7b29f5918b254524cdcb865c
labib <labib.hsn.bd@gmail.com>
2026-04-19 20:51:50 +0600

    P_35_Minutes_Before_the_New_Year

---
+++ ONLINE_JUDGE/CODEFORCES/!00EASY_problem/P_35_Minutes_Before_the_New_Year.cpp
@@ -0,0 +1,31 @@
+///   ***   ---   ||   SOLVED BY LABIB   ||   ---   ***   ///
+///   ***   ---   ||   DATE: 19/04/2026  ||   ---   ***   ///
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define debug(x) cout << #x << ": " << x << endl;
+
+void solve() {
+    int h, m; cin >> h >> m;
+    cout << 1440-((h*60)+m) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); 
+    cout.tie(0);
+
+    int t;
+    cin >> t;  
+    while(t--){ 
+        solve();
+    }
+    return 0;
+}
